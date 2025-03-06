949c73cfee282a5609cb5ef86c2a072edf7c7535
alinargit <127021881+alinargit@users.noreply.github.com>
2025-03-06 19:12:51 +0100

    Initial commit

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html <?php language_attributes();?>>
+<head>
+    <meta charset="<?php bloginfo('charset')?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <p>Lorem ipsum is a dummy or placeholder text commonly used in graphic design, publishing, 
+        and web development to fill empty spaces in a layout that does not yet have content. 
+        It consists of a sequence of Latin words that do not form complete sentences but serve 
+        as placeholders until actual content is added.</p>
+        <header>
+            <section class="top-bar">
+                <div class="logo">
+                    Logo
+                </div>
+                <div class="search-box">
+                    Search
+                </div>
+            </section>
+            <section class="menu-area">
+                <nav class="main-menu">
+                    Menu
+                </nav>
+            </section>
+        </header>
