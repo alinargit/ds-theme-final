0074b8dc5482e1a907c3a90b94cf81c1db815b12
alinargit <127021881+alinargit@users.noreply.github.com>
2025-03-11 19:16:54 +0100

    d

--- header.php
+++ header.php
@@ -6,10 +6,6 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-    <p>Lorem ipsum is a dummy or placeholder text commonly used in graphic design, publishing, 
-        and web development to fill empty spaces in a layout that does not yet have content. 
-        It consists of a sequence of Latin words that do not form complete sentences but serve 
-        as placeholders until actual content is added.</p>
         <header>
             <section class="top-bar">
                 <div class="logo">
@@ -21,7 +17,8 @@
             </section>
             <section class="menu-area">
                 <nav class="main-menu">
-                    Menu
+                    <?php wp_nav_menu(
+                        array('theme_location' => 'wp_devs_main_menu', 'depth' => 2));?>
                 </nav>
             </section>
         </header>
