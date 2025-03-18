d039ea05bc18c9776e587b0b1eaa0e08428ace6d
alinargit <127021881+alinargit@users.noreply.github.com>
2025-03-18 19:15:09 +0100

    ss

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,1 +1,12 @@
-console.log("Testing");
+let dropdown = document.querySelector('.menu'), //ul
+submenu = document.querySelector('.sub-menu'), //ul li a
+buttonclick = document.querySelector('.check-button'), //button
+hamburger = document.querySelector(',menu-icon');
+
+buttonclick.addEventListener('click', ()=>{
+    dropdown.classList.toggle('shop-dropdown');
+    if(submenu){
+        submenu.classList.toggle('shop-dropdown');
+    }
+    hamburger.classList.toggle('animate-button');
+})
