2d688346f85b6cd55ea95fcfa95912b6df9ddec8
alinargit <127021881+alinargit@users.noreply.github.com>
2025-03-25 19:18:20 +0100

    d

--- functions.php
+++ functions.php
@@ -38,6 +38,17 @@ add_action('widgets_init', 'dstheme_sidebars');
 function dstheme_sidebars(){
     register_sidebar(
         array(
+            register_sidebar(
+                array(
+                    'name' => 'Blog Sidebar',
+                    'id' => 'sidebar-blog',
+                    'description' => 'This is the Blog Sidebar. You can add your widgets here.',
+                    'before_widget' => '<div class = "widget-title">',
+                    'after_widget' => '</div>',
+                    'before_title' => '<h4 class = "widget-title">',
+                    'after_title' => '</h4>'
+                )
+            ); 
             'name' => 'Service 1',
             'id' => 'services-1',
             'description' => 'First service Area',
